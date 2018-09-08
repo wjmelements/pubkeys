8d7280bfc4387eb313a1f68bcc6e828a8134ef69
William Morriss <ec2-user@ip-172-31-36-211.us-west-2.compute.internal>
2018-09-08 02:33:27 +0000

    fix 600 and add ragnar.pem

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ function die() {
 
 mkdir -p ~/.ssh
 cat ssh/* > authorized_keys
+chmod 600 authorized_keys
 diff --unidirectional-new-file ~/.ssh/authorized_keys authorized_keys && die "All keys already installed"
 read -p "Are you sure? [Y/n] " -n 1 -r
 echo # newline
